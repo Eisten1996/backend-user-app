afc90b2db9ad1f25652490322f051a4a502109e3
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-04 18:21:28 -0500

    morgan y cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(cors());
 
 module.exports = app;
-
-// const port = process.env.PORT || port;
-// const www = process.env.WWW || './';
-// app.use(express.static(www));
-// console.log(`serving ${www}`);
-// app.get('*', (req, res) => {
-//     res.sendFile(`index.html`, { root: www });
-// });
-// app.listen(port, () => console.log(`listening on http://localhost:${port}`));
