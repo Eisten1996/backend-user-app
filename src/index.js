2d74f08d6ce43265413de5bb6e623817ede18b38
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-04 18:33:45 -0500

    conexion de bd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+const { connection } = require('mongoose');
 const app = require('./app');
+const { connect } = require('./database');
 
 async function main() {
+  // Database connection
+  await connect();
+  // Express Application
   await app.listen(4000);
-  console.log('Server on port 4000');
+  console.log('Server on port 4000: Connected');
 }
 
 main();
