2ebfecd8c4e4ddae7e994d07b7d82f05e2deb65f
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-04 18:16:02 -0500

    creacion de server en puerto 4000

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+module.exports = app;
+
+// const port = process.env.PORT || port;
+// const www = process.env.WWW || './';
+// app.use(express.static(www));
+// console.log(`serving ${www}`);
+// app.get('*', (req, res) => {
+//     res.sendFile(`index.html`, { root: www });
+// });
+// app.listen(port, () => console.log(`listening on http://localhost:${port}`));
