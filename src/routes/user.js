006f8ef9dfd5c8bc4f52488657dfdc534cbe4f7a
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-04 19:03:55 -0500

    usando faker

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,22 @@
 const { Router } = require('express');
-const { model } = require('mongoose');
-
 const route = Router();
+const faker = require('faker');
+const User = require('../models/User');
+
+route.get('/api/users', async (req, res) => {
+  const users = await User.find();
+  res.json({ users });
+});
+
+route.get('/api/users/create', async (req, res) => {
+  for (let i = 0; i < 5; i++) {
+    await User.create({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      avatar: faker.image.avatar(),
+    });
+  }
+  res.json({ message: '5 Users created' });
+});
 
 module.exports = route;
